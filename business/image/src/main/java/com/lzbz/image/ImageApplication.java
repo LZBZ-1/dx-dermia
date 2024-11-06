bb379f5cc169bd3e792658b58d271e2b53c97d78
LZBZ <jalb200312@gmail.com>
2024-11-06 09:24:51 -0500

    Add dependency image

---
+++ business/image/src/main/java/com/lzbz/image/ImageApplication.java
@@ -0,0 +1,15 @@
+package com.lzbz.image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ImageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImageApplication.class, args);
+    }
+
+}
